94cc7a7f080388f05deeab51b37a861759bcb946
zaizen24 <achmadzainunq@gmail.com>
2023-09-24 20:44:45 +0700

    first commit

---
+++ main.js
@@ -0,0 +1,23 @@
+// Slider
+var slides = document.querySelectorAll(".slide");
+var btns = document.querySelectorAll(".btn");
+let currentSlide = 1;
+
+var manualNav = function (manual) {
+  slides.forEach((slide) => {
+    slide.classList.remove("active");
+    btns.forEach((btn) => {
+      btn.classList.remove("active");
+    });
+  });
+
+  slides[manual].classList.add("active");
+  btns[manual].classList.add("active");
+};
+
+btns.forEach((btn, i) => {
+  btn.addEventListener("click", () => {
+    manualNav(i);
+    currentSlide = i;
+  });
+});
